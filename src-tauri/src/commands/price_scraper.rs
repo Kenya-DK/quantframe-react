5a0c68fdbde92dc5cec740a77ab707107efe00a8
Kenya <kenni.k@hotmail.com>
2023-11-06 01:13:18 +0100

    clean up

--- src-tauri/src/commands/price_scraper.rs
+++ src-tauri/src/commands/price_scraper.rs
@@ -1,9 +1,11 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
-use crate::{error::AppError, price_scraper::PriceScraper};
+use once_cell::sync::Lazy;
+
+use crate::{error::{AppError, self}, price_scraper::PriceScraper};
 
 // Create a static variable to store the log file name
-// static LOG_FILE: Lazy<Mutex<String>> = Lazy::new(|| Mutex::new("app.log".to_string()));
+static LOG_FILE: Lazy<Mutex<String>> = Lazy::new(|| Mutex::new("price_history.log".to_string()));
 
 #[tauri::command(async)]
 pub async fn generate_price_history(
@@ -11,5 +13,11 @@ pub async fn generate_price_history(
     price_scraper: tauri::State<'_, Arc<std::sync::Mutex<PriceScraper>>>,
 ) -> Result<i64, AppError> {
     let price_scraper = price_scraper.lock().unwrap().clone();
-    Ok(price_scraper.generate(days).await?)
+    match price_scraper.generate(days).await {
+        Ok(_) => Ok(0),
+        Err(e) => {
+            error::create_log_file(LOG_FILE.lock().unwrap().to_owned(), &e);
+            return Err(e)
+        }
+    }
 }
