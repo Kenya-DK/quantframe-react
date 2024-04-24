1c2733cb6ccc322306d44531fd1128d44be1673f
jovanaaaaajovan <97354542+jovanaaaaajovan@users.noreply.github.com>
2024-04-24 14:24:13 +0200

    Minor skill issue on my end

--- src-tauri/src/live_scraper/modules/item.rs
+++ src-tauri/src/live_scraper/modules/item.rs
@@ -503,6 +503,19 @@ impl ItemModule {
                 unselected_items.push(items[i].clone());
             }
         }
+        
+        // In the `items` parameter, the last element is always not on Warframe Market (the one currently getting checked),
+        // so it should be added only if it's not already posted, unless the price would go over the max price cap limit. 
+        // Because if it is posted and gets added in unselected_items, 
+        // it will be expecting an order_id because the item is posted on Warframe Market.
+        if !selected_items.iter().any(|&(_, _, ref name, _)| name == &items[n-1].2) {
+            if w - items[n-1].0 < 0 {
+                unselected_items.push(items[n-1].clone());
+            } else {
+                selected_items.push(items[n-1].clone());
+            }
+        }
+
 
         Ok((dp[n], selected_items, unselected_items))
     }  
