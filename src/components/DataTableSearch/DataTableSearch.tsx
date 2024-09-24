3d36ac86f4eee311f449dbd9de7c6724d5a20b48
Kenya-DK <kenni.k@hotmail.com>
2024-09-24 16:25:35 +0200

    remove some debug

--- src/components/DataTableSearch/DataTableSearch.tsx
+++ src/components/DataTableSearch/DataTableSearch.tsx
@@ -5,44 +5,67 @@ import { useEffect, useState } from "react";
 import { sortArray } from "@utils/sorting.helper";
 import { paginate } from "@utils/helper";
 import { SearchField, SearchFieldProps } from "../SearchField";
-import { searchProperties,Query } from "@utils/search.helper";
+import { searchProperties, Query } from "@utils/search.helper";
 
 export type DataTableSearchProps<T> = {
   filters?: Query;
   sorting?: SortingField;
   query?: string;
   onSearchChange?: (query: string) => void;
-} & DataTableProps<T> & Omit<SearchFieldProps, "value" | "onChange">;
-export const DataTableSearch = <T,>({ query, onSearchChange, records, rightSection, rightSectionWidth, filters, filter, sorting, ...otherProps }: DataTableSearchProps<T>) => {
-
+} & DataTableProps<T> &
+  Omit<SearchFieldProps, "value" | "onChange">;
+export const DataTableSearch = <T,>({
+  query,
+  onSearchChange,
+  records,
+  rightSection,
+  rightSectionWidth,
+  filters,
+  filter,
+  sorting,
+  ...otherProps
+}: DataTableSearchProps<T>) => {
   // States For DataGrid
   const [page, setPage] = useState(1);
   const pageSizes = [5, 10, 15, 20, 25, 30, 50, 100];
   const [pageSize, setPageSize] = useState(pageSizes[4]);
   const [rows, setRows] = useState<T[]>([]);
   const [totalRecords, setTotalRecords] = useState<number>(0);
-  const [sortStatus, setSortStatus] = useState<DataTableSortStatus<T>>({ columnAccessor: sorting?.field || "", direction: sorting?.direction || "asc" });
+  const [sortStatus, setSortStatus] = useState<DataTableSortStatus<T>>({
+    columnAccessor: sorting?.field || "",
+    direction: sorting?.direction || "asc",
+  });
   // Update Database Rows
   useEffect(() => {
-    if (!records)
-      return;
+    if (!records) return;
 
     let filteredRecords = records;
-    console.log("DataTableSearch: Filter Status", filteredRecords.map((x) => (x as any).id));
 
-    filteredRecords = sortArray([{
-      field: sortStatus.columnAccessor as string,
-      direction: sortStatus.direction
-    }], filteredRecords);
-    if (filters)
-      filteredRecords = searchProperties(records, filters, false);
+    filteredRecords = sortArray(
+      [
+        {
+          field: sortStatus.columnAccessor as string,
+          direction: sortStatus.direction,
+        },
+      ],
+      filteredRecords
+    );
+    if (filters) filteredRecords = searchProperties(records, filters, false);
     setTotalRecords(filteredRecords.length);
     filteredRecords = paginate(filteredRecords, page, pageSize);
     setRows(filteredRecords);
   }, [filters, page, pageSize, sortStatus]);
   return (
-    <Box pos="relative" >
-      {(onSearchChange && query != undefined) && <SearchField value={query} onChange={(t) => onSearchChange(t)} rightSection={rightSection} rightSectionWidth={rightSectionWidth} filter={filter} />}
+    <Box pos="relative">
+      {onSearchChange && query != undefined && (
+        <SearchField
+          value={query}
+          onChange={(t) => onSearchChange(t)}
+          rightSection={rightSection}
+          rightSectionWidth={rightSectionWidth}
+          filter={filter}
+        />
+      )}
       <DataTable
         mt={5}
         records={rows}
@@ -56,7 +79,6 @@ export const DataTableSearch = <T,>({ query, onSearchChange, records, rightSecti
         onSortStatusChange={setSortStatus}
         {...otherProps}
       />
-
     </Box>
-  )
-}
+  );
+};
