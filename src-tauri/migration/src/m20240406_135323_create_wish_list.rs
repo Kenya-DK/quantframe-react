8e01c457fb77bc6628e8e6ffdcdc9768d48ae1bd
Kenya69 <kenni.k@hotmail.com>
2024-10-30 15:04:03 +0100

    wishlist

---
+++ src-tauri/migration/src/m20240406_135323_create_wish_list.rs
@@ -0,0 +1,88 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(WishList::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(WishList::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(WishList::CreatedAt).date_time().not_null())
+                    .col(ColumnDef::new(WishList::UpdatedAt).date_time().not_null())
+                    .col(ColumnDef::new(WishList::WFMId).uuid().not_null())
+                    .col(ColumnDef::new(WishList::WFMUrl).string().not_null())
+                    .col(ColumnDef::new(WishList::ItemName).string().not_null())
+                    .col(
+                        ColumnDef::new(WishList::ItemUniqueName)
+                            .string()
+                            .not_null()
+                            .default("N/A"),
+                    )
+                    .col(ColumnDef::new(WishList::SubType).json())
+                    .col(
+                        ColumnDef::new(WishList::Quantity)
+                            .integer()
+                            .not_null()
+                            .default(Value::Int(Some(1))),
+                    )
+                    .col(ColumnDef::new(WishList::MaximumPrice).integer())
+                    .col(ColumnDef::new(WishList::ListPrice).integer())
+                    .col(
+                        ColumnDef::new(StockItem::PriceHistory)
+                            .json()
+                            .not_null()
+                            .default("[]"),
+                    )
+                    .col(
+                        ColumnDef::new(WishList::Status)
+                            .string()
+                            .not_null()
+                            .default("pending"),
+                    )
+                    .to_owned(),
+                    
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .drop_table(Table::drop().table(WishList::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum WishList {
+    Table,
+    Id,
+    CreatedAt,
+    UpdatedAt,
+    WFMId,
+    WFMUrl,
+    ItemName,
+    ItemUniqueName,
+    SubType,
+    Quantity,
+    MaximumPrice,
+    ListPrice,
+    PriceHistory,
+    Status,
+}
