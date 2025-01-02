eb5fd07ed052c3b2ccbef7cc8f1bcc7a2964e50b
Kenya-DK <kenni.k@hotmail.com>
2025-01-02 23:47:35 +0100

    lang WIP

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -11,42 +11,15 @@ export const en = {
         message: "An error occurred in {{component}} at {{backtrace}} see logs for more information.",
       },
     },
-    on_trade_event: {
-      success: {
-        riven: {
-          title: "Trade with {{player_name}} complete",
-          message: "x{{quantity}} {{item_name}}",
-        },
-        item: {
-          title: "Trade with {{player_name}} complete",
-          message: "x{{quantity}} {{item_name}} Order:{{order}} Stock:{{stock}} Transaction:{{transaction}}",
-        },
-      },
+    created_stock: {
       warning: {
-        riven: {
-          title: "Trade Warning",
-          message: "⚠️ Trade was not processed.",
-        },
-        item: {
-          title: "Trade Warning",
-          message: "⚠️ Trade was not processed.",
-        },
-        created_stock: {
-          title: "Trade Warning",
-          message: "⚠️ Trade was not processed.",
-        },
-      },
-      error: {
-        item: {
-          title: "Trade Error",
-          message: "An error occurred while trying to trade item {{name}}.",
-        },
-        riven: {
-          title: "Trade Error",
-          message: "An error occurred while trying to trade riven {{name}}.",
+        not_found: {
+          title: "Item Not Found",
+          message: "Item {{item}} was not found.",
         },
       },
     },
+    on_trade_event: {},
   },
   enums: {
     transaction_type: {
