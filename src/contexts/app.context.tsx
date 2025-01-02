74cf089e3a1611babd932533446ac1f495d47f47
Kenya-DK <kenni.k@hotmail.com>
2025-01-02 23:47:00 +0100

    clean up

--- src/contexts/app.context.tsx
+++ src/contexts/app.context.tsx
@@ -4,33 +4,34 @@ import api, { OnTauriDataEvent, OnTauriEvent } from "@api/index";
 import { useTranslateContexts, useTranslateNotifications } from "@hooks/useTranslate.hook";
 import { notifications } from "@mantine/notifications";
 import { Box, Button, Group } from "@mantine/core";
-import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+import { checkUpdate, installUpdate } from "@tauri-apps/api/updater";
 import { relaunch } from "@tauri-apps/api/process";
 import { AppInfo, QfSocketEvent, QfSocketEventOperation, ResponseError, Settings } from "@api/types";
 import { AuthContextProvider } from "./auth.context";
 import { SplashScreen } from "@components/SplashScreen";
 import { TextTranslate } from "@components/TextTranslate";
 import { modals } from "@mantine/modals";
-import { RichTextEditor } from '@mantine/tiptap';
-import { useEditor } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import { Markdown } from 'tiptap-markdown';
+import { RichTextEditor } from "@mantine/tiptap";
+import { useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { Markdown } from "tiptap-markdown";
 
 type NotificationPayload = {
   i18n_key_title: string;
   i18n_key_message: string;
+  autoClose?: boolean | number;
   values: { [key: string]: string | number };
-}
+};
 
 export type AppContextProps = {
   settings: Settings | undefined;
   app_info: AppInfo | undefined;
   app_error?: ResponseError;
-}
+};
 
 export type AppContextProviderProps = {
   children: React.ReactNode;
-}
+};
 
 export const AppContext = createContext<AppContextProps>({
   settings: undefined,
@@ -43,72 +44,82 @@ export const useAppContext = () => useContext(AppContext);
 export function AppContextProvider({ children }: AppContextProviderProps) {
   const [settings, setSettings] = useState<Settings | undefined>(undefined);
   const [appInfo, setAppInfo] = useState<AppInfo | undefined>(undefined);
-  const [i18Key, setI18Key] = useState<string>('cache');
+  const [i18Key, setI18Key] = useState<string>("cache");
   const [isControl, setIsControl] = useState<boolean>(false);
   const [appError, setAppError] = useState<ResponseError | undefined>(undefined);
   // Translate general
-  const useTranslate = (key: string, context?: { [key: string]: any }, i18Key?: boolean) => useTranslateContexts(`app.${key}`, { ...context }, i18Key)
-  const useTranslateEvents = (key: string, context?: { [key: string]: any }, i18Key?: boolean) => useTranslate(`loading_events.${key}`, { ...context }, i18Key)
-  const useTranslateNewUpdate = (key: string, context?: { [key: string]: any }, i18Key?: boolean) => useTranslate(`new_update.${key}`, { ...context }, i18Key)
-  const useTranslateNewUpdateButtons = (key: string, context?: { [key: string]: any }, i18Key?: boolean) => useTranslateNewUpdate(`buttons.${key}`, { ...context }, i18Key);
+  const useTranslate = (key: string, context?: { [key: string]: any }, i18Key?: boolean) =>
+    useTranslateContexts(`app.${key}`, { ...context }, i18Key);
+  const useTranslateEvents = (key: string, context?: { [key: string]: any }, i18Key?: boolean) =>
+    useTranslate(`loading_events.${key}`, { ...context }, i18Key);
+  const useTranslateNewUpdate = (key: string, context?: { [key: string]: any }, i18Key?: boolean) =>
+    useTranslate(`new_update.${key}`, { ...context }, i18Key);
+  const useTranslateNewUpdateButtons = (key: string, context?: { [key: string]: any }, i18Key?: boolean) =>
+    useTranslateNewUpdate(`buttons.${key}`, { ...context }, i18Key);
 
   // Fetch data from rust side
   const { isFetching, error } = useQuery({
-    queryKey: ['app_init'],
+    queryKey: ["app_init"],
     queryFn: () => api.app.init(),
     retry: 0,
-    enabled: !window.location.href.includes('controls'),
-  })
+    enabled: !window.location.href.includes("controls"),
+  });
 
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Markdown,
-    ],
+    extensions: [StarterKit, Markdown],
     content: ``,
-  })
+  });
   useEffect(() => {
-    setIsControl(window.location.href.includes('controls'));
+    setIsControl(window.location.href.includes("controls"));
 
     OnTauriEvent(QfSocketEvent.OnInitialize, (i18Key: string) => setI18Key(i18Key));
 
-    checkUpdate().then(({ shouldUpdate, manifest }) => {
-      if (!shouldUpdate || !manifest || !editor)
-        return;
-      editor.commands.setContent(manifest?.body);
-      modals.open({
-        title: useTranslateNewUpdate("title", { version: manifest?.version }),
-        size: 'lg',
-        children: (<>
-          <RichTextEditor editor={editor}>
-            <RichTextEditor.Content />
-          </RichTextEditor>
-          <Group grow justify="space-between" mt={"md"}>
-            <Button onClick={async () => {
-              // Install the update. This will also restart the app on Windows!
-              await installUpdate();
+    checkUpdate()
+      .then(({ shouldUpdate, manifest }) => {
+        if (!shouldUpdate || !manifest || !editor) return;
+        editor.commands.setContent(manifest?.body);
+        modals.open({
+          title: useTranslateNewUpdate("title", { version: manifest?.version }),
+          size: "lg",
+          children: (
+            <>
+              <RichTextEditor editor={editor}>
+                <RichTextEditor.Content />
+              </RichTextEditor>
+              <Group grow justify="space-between" mt={"md"}>
+                <Button
+                  onClick={async () => {
+                    // Install the update. This will also restart the app on Windows!
+                    await installUpdate();
 
-              // On macOS and Linux you will need to restart the app manually.
-              // You could use this step to display another confirmation dialog.
-              await relaunch();
-            }}>{useTranslateNewUpdateButtons('install')}</Button>
-            <Button onClick={async () => {
-              window.open(`https://github.com/Kenya-DK/quantframe-react/releases/tag/v${manifest.version}`, '_blank');
-            }}>{useTranslateNewUpdateButtons('read_more')}</Button>
-          </Group>
-        </>),
+                    // On macOS and Linux you will need to restart the app manually.
+                    // You could use this step to display another confirmation dialog.
+                    await relaunch();
+                  }}
+                >
+                  {useTranslateNewUpdateButtons("install")}
+                </Button>
+                <Button
+                  onClick={async () => {
+                    window.open(`https://github.com/Kenya-DK/quantframe-react/releases/tag/v${manifest.version}`, "_blank");
+                  }}
+                >
+                  {useTranslateNewUpdateButtons("read_more")}
+                </Button>
+              </Group>
+            </>
+          ),
+        });
+      })
+      .catch((e) => {
+        console.error(e);
       });
-
-    }).catch((e) => {
-      console.error(e);
-    })
-  }, [])
-
+  }, []);
 
   useEffect(() => {
     if (error == undefined) return;
     setAppError(error as ResponseError);
-  }, [error])
+  }, [error]);
   // Handle update, create, delete
   const handleUpdateSettings = (operation: QfSocketEventOperation, data: Settings) => {
     switch (operation) {
@@ -119,7 +130,7 @@ export function AppContextProvider({ children }: AppContextProviderProps) {
         setSettings(data);
         break;
     }
-  }
+  };
   const handleUpdateAppInfo = (operation: QfSocketEventOperation, data: AppInfo) => {
     switch (operation) {
       case QfSocketEventOperation.CREATE_OR_UPDATE:
@@ -129,33 +140,34 @@ export function AppContextProvider({ children }: AppContextProviderProps) {
         setAppInfo(data);
         break;
     }
-  }
-  const handleNotification = (payload: NotificationPayload, color: string) => {
+  };
+  const handleNotification = (payload: NotificationPayload, color: string, autoClose: boolean = true) => {
     notifications.show({
       title: useTranslateNotifications(payload.i18n_key_title, payload.values),
-      message: <Box>
-        <TextTranslate i18nKey={useTranslateNotifications(payload.i18n_key_message, undefined, true)} values={payload.values} />
-      </Box>,
+      autoClose: payload.autoClose ?? autoClose,
+      message: (
+        <Box>
+          <TextTranslate i18nKey={useTranslateNotifications(payload.i18n_key_message, undefined, true)} values={payload.values} />
+        </Box>
+      ),
       color: color,
     });
-  }
+  };
   // Hook on tauri events from rust side
   useEffect(() => {
     OnTauriDataEvent<Settings>(QfSocketEvent.UpdateSettings, ({ data, operation }) => handleUpdateSettings(operation, data));
     OnTauriDataEvent<AppInfo>(QfSocketEvent.UpdateAppInfo, ({ data, operation }) => handleUpdateAppInfo(operation, data));
     OnTauriEvent<ResponseError>(QfSocketEvent.UpdateAppError, (data) => setAppError(data));
-    OnTauriEvent<NotificationPayload>(QfSocketEvent.OnNotificationError, (data) => handleNotification(data, 'red.7'));
-    OnTauriEvent<NotificationPayload>(QfSocketEvent.OnNotificationWarning, (data) => handleNotification(data, 'yellow.7'));
-    OnTauriEvent<NotificationPayload>(QfSocketEvent.OnNotificationSuccess, (data) => handleNotification(data, 'green.7'));
-    return () => { }
+    OnTauriEvent<NotificationPayload>(QfSocketEvent.OnNotificationError, (data) => handleNotification(data, "red.7", false));
+    OnTauriEvent<NotificationPayload>(QfSocketEvent.OnNotificationWarning, (data) => handleNotification(data, "yellow.7", false));
+    OnTauriEvent<NotificationPayload>(QfSocketEvent.OnNotificationSuccess, (data) => handleNotification(data, "green.7"));
+    return () => {};
   }, []);
 
   return (
     <AppContext.Provider value={{ settings, app_info: appInfo, app_error: appError }}>
       {!isControl && <SplashScreen opened={isFetching} text={useTranslateEvents(i18Key)} />}
-      <AuthContextProvider>
-        {children}
-      </AuthContextProvider>
+      <AuthContextProvider>{children}</AuthContextProvider>
     </AppContext.Provider>
-  )
+  );
 }
