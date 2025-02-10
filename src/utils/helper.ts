ba7af08b74adf02ade160653e479b519a0c07d3b
Kenya-DK <kenni.k@hotmail.com>
2025-02-10 23:17:01 +0100

    remove some more debug

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -126,7 +126,6 @@ export const getCssVariable = (name: string) => {
 
 export const GetSubTypeDisplay = (subType: SubType | undefined) => {
   if (!subType || Object.keys(subType).length == 0) return "";
-  debugger;
   const { rank, variant, amber_stars, cyan_stars } = subType;
   let display = "";
   if (rank != undefined) display += `(R${rank})`;
