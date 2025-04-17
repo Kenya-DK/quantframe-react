af46246c9c0c8d0d7ed9539a7d489dcd3ac99edc
Kenya-DK <kenni.k@hotmail.com>
2025-04-17 10:54:16 +0200

    Updates API endpoint and pagination
    Sets the default API endpoint to the local development server.
    
    Also, corrects the default page number for fetching alerts, changing it from 0 to 1, aligning with expected pagination behavior.

--- src-tauri/src/qf_client/client.rs
+++ src-tauri/src/qf_client/client.rs
@@ -54,8 +54,7 @@ impl QFClient {
     pub fn new() -> Self {
         QFClient {
             endpoint: "https://api.quantframe.app/".to_string(),
-            endpoint_dev: "https://api.quantframe.app/".to_string(),
-            // endpoint_dev: "http://localhost:6969/".to_string(),
+            endpoint_dev: "http://localhost:6969/".to_string(),
             is_dev: if cfg!(debug_assertions) { true } else { false },
             limiter: Arc::new(tokio::sync::Mutex::new(RateLimiter::new(
                 3.0,
