2518a76f8b90a747c72d8f0742811517db9b7f6e
Jacob <80339878+JacobPlaysGames@users.noreply.github.com>
2025-06-10 12:16:15 -0400

    Various Fixes/Tweaks
    - Fix user menu displaying off screen
    
    - Update riven logic to properly filter based on similarity threshold
    
    - Update riven logic to properly ignore all filters when the filter is disabled
    
    - Update riven logic to use a minimum price shift instead of a maximum
    
    - Update riven attribute display to include -/+, x/%, and better colors for readability

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -255,7 +255,7 @@ export const en = {
                 label: "Minimum Price Shift",
                 placeholder: "0",
                 error: "Invalid threshold percentage",
-                tooltip: "Ignore rivens that have less than {{value}}% price shift from the most expensive riven",
+                tooltip: "Ignore rivens that have more than {{value}}% price shift from the least expensive riven",
               },
             },
             tabs: {
@@ -792,7 +792,7 @@ export const en = {
       },
     },
     riven_attribute: {
-      effect: "{{value}} {{name}}",
+      effect: "<num_color_tag>{{value}}</num_color_tag> {{name}}",
     },
     tradableItem_list: {
       pagination: "{{start}} - {{end}} / {{totalRecords}}",
