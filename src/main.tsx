83f70d2791f26aec82b37b9209300109bf40031e
Kenya-DK <kenni.k@hotmail.com>
2025-08-06 15:57:37 +0200

    Removes StrictMode from the application
    Removes the StrictMode wrapper from the root component.
    
    StrictMode can cause double-rendering of components in development,
    which is not needed now.

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,5 @@ import "@mantine/tiptap/styles.css";
 import "@mantine/dates/styles.css";
 import "./global.css";
 import App from "./App.tsx";
-import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
