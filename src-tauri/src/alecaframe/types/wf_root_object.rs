17566099dbbf9df1e4a63ee987d58fc140703cfa
Kenya69 <kenni.k@hotmail.com>
2026-02-09 15:03:56 +0100

    Adds more warframe profile data
    Adds fields to represent the user's profile data,
    such as mastery rank, trade limit, and various item lists.
    
    Includes a default implementation to ensure that missing data
    does not cause errors.

--- src-tauri/src/alecaframe/types/wf_root_object.rs
+++ src-tauri/src/alecaframe/types/wf_root_object.rs
@@ -2,17 +2,36 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct WarframeRootObject {
+    #[serde(rename = "PlayerLevel", default)]
+    pub mastery_rank: i64,
+
     #[serde(rename = "PremiumCredits", default)]
     pub platinum: i64,
 
     #[serde(rename = "RegularCredits", default)]
     pub credits: i64,
+
+    #[serde(rename = "TradesRemaining", default)]
+    pub trades_remaining: i64,
+
+    #[serde(rename = "RawUpgrades", default)]
+    pub raw_upgrades: Vec<MiscItem>,
+
+    #[serde(rename = "Upgrades", default)]
+    pub upgrades: Vec<MiscItem>,
+
+    #[serde(rename = "Recipes", default)]
+    pub recipes: Vec<MiscItem>,
 }
 impl Default for WarframeRootObject {
     fn default() -> Self {
         Self {
             platinum: 0,
             credits: 0,
+            trades_remaining: 0,
+            raw_upgrades: vec![],
+            upgrades: vec![],
+            recipes: vec![],
         }
     }
 }
